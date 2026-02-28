9a0b573e031ce4a1db5fb0126e9e7b5ed7c64bdc
Radman <radikahn@gmail.com>
2026-02-28 02:05:45 -0800

    crying again

--- src/routes/cart.tsx
+++ src/routes/cart.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { createFileRoute, useNavigate } from '@tanstack/react-router'
 import type { CartItem } from '#/components/MainPage/ItemFeature'
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
+const API_URL = import.meta.env.VITE_API_URL || ''
 
 const affiliations = ['SJSU Student', 'SJSU Alumni', 'Friend of SAEC'] as const
 
